b5b14aee3c6a932691482e47dc4fa7a88671365a
Harsh Mahajan <harsh@Harshs-MacBook-Pro.local>
2024-06-23 16:49:15 -0600

    cricketer questions added

--- groq_inference.py
+++ groq_inference.py
@@ -25,14 +25,14 @@ def generate_response(model,prompt):
     )
     return response
 
-with open('data/gov_agencies/gov_agencies_questions.json','r') as f:
+with open('data/economy/economy_questions.json','r') as f:
     entities = json.load(f)
 total_questions = 0
 for entity in entities:
     folder_question = 0
     entity_name = entity.split(" ")
     entity_name = "_".join(entity_name)
-    with open(f'data/gov_agencies/{entity_name}.json','r') as f:
+    with open(f'data/economy/{entity_name}.json','r') as f:
         timeline = json.load(f)
     if len(timeline)>20:
         continue
@@ -48,18 +48,27 @@ for entity in entities:
             try:
                 response = generate_response("mixtral-8x7b-32768",prompt)
             except:
-                time.sleep(90)
+                time.sleep(30)
                 response = generate_response("mixtral-8x7b-32768",prompt)
-            predicted_ans = response.choices[0].message.content
+            predicted_ans = ""
+            try:
+                predicted_ans = response.choices[0].message.content
+            except:
+                try:
+                    time.sleep(70)
+                    response = generate_response("mixtral-8x7b-32768", prompt)
+                except:
+                    print(entity, questions_category, actual_question)
+                    continue
             actual_ans = entities[entity][questions_category][question]["A"]
             df["entity"].append(entity)
             df["question"].append(entities[entity][questions_category][question]["Q"])
             df["actual_answer"].append(actual_ans)
             df["predicted_answer"].append(predicted_ans)
-            time.sleep(10)
+            time.sleep(5)
             folder_question += 1
     total_questions += folder_question
     print(f"{entity} -- {folder_question}")
     dataframe = pd.DataFrame(df)
-    dataframe.to_csv(f"data/gov_agencies/predictions/mixtral-8*7b/{entity}.csv", index=False)
+    dataframe.to_csv(f"data/economy/predictions/mixtral-8*7b/{entity}.csv", index=False)
 print(f"total - {total_questions}")
